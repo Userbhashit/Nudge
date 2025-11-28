776e40f1c659648a51201d0eeb0cac4fbd64eadc
Bhashit Gautam <bhashitgtm004@gmail.com>
2025-11-28 19:54:45 +0530

    Updated main to call getflag function

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,17 @@
 #include "setup.hpp"
+#include "flags.hpp"
 
-int main() {
+int main(int argc, char* argv[]) {
   
+  // Setup database and config directory. 
   initializeApplication();
-  
+
+  // Get command and description.
+  std::string description("");
+  Flag command = getFlag(argc, argv, description);
+  executeFlag(command, description);
+
+
   return 0;
 }
 
